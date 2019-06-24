beb3580da2e21ce5b4b428f2172a6a20020266ac
dntutty <15755518513@qq.com>
2019-06-24 10:04:26 +0800

    feat(Density.java):屏幕密度适配

---
+++ app/src/main/java/com/dntutty/ui/screenadapter/density/Density.java
@@ -0,0 +1,34 @@
+package com.dntutty.ui.screenadapter.density;
+
+import android.app.Activity;
+import android.app.Application;
+import android.util.DisplayMetrics;
+
+public class Density {
+
+    private static final float WIDTH = 360;//参考设备的宽度,单位是dp
+
+    private static float appDensity;//表示屏幕密度
+    private static float appScaleDensity;//字体缩放比例,默认appDensity;
+
+    public static void setDensity(Application application, Activity activity) {
+        //获取当前app的屏幕显示信息
+        DisplayMetrics displayMetrics = application.getResources().getDisplayMetrics();
+        if (appDensity == 0) {
+            //初始化赋值操作
+            appDensity = displayMetrics.density;
+            appScaleDensity = displayMetrics.scaledDensity;
+        }
+
+        //计算目标值density,scaleDensity,densityDpi
+        float targetDensity = displayMetrics.widthPixels / WIDTH;//1080/360 = 3.0
+        float targetScaleDensity = targetDensity * (appScaleDensity / appDensity);
+        int targetDensityDpi = (int) (targetDensity * 160);
+
+        //替换Activity的density,scaleDensity,densityDpi
+        DisplayMetrics dm = activity.getResources().getDisplayMetrics();
+        dm.density = targetDensity;
+        dm.scaledDensity = targetScaleDensity;
+        dm.densityDpi = targetDensityDpi;
+    }
+}
